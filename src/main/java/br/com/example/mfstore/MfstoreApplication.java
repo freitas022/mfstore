f92bca483e2ea8d3a1c0dbaebdaa7dfe65af0e7e
Matheus Freitas <mf022rj@gmail.com>
2026-03-31 14:46:52 -0300

    Starter project

---
+++ src/main/java/br/com/example/mfstore/MfstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.mfstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfstoreApplication.class, args);
+	}
+
+}
