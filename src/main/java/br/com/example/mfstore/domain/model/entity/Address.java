f90e82937d13c85c6bebec455a682f675ba5f2f7
Matheus Freitas <mf022rj@gmail.com>
2026-04-01 15:58:34 -0300

    add one-to-many association Customer with Address

---
+++ src/main/java/br/com/example/mfstore/domain/model/entity/Address.java
@@ -0,0 +1,35 @@
+package br.com.example.mfstore.domain.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_address")
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false, length = 8)
+    private String zipCode;
+
+    private String complement;
+}
---
+++ src/main/java/br/com/example/mfstore/domain/model/entity/Customer.java
@@ -0,0 +1,37 @@
+package br.com.example.mfstore.domain.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_customer")
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+public class Customer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 11, unique = true)
+    private String phone;
+
+    @Column(nullable = false, length = 11, unique = true)
+    @EqualsAndHashCode.Include
+    private String document;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "customer_id")
+    @Builder.Default
+    private List<Address> addresses = new ArrayList<>();
+}
